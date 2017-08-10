0ea1afd7215fe4743fc71c63b6d944a14f402fc6
shri2c2p <shri@2c2p.com>
2017-08-10 13:50:50 +0530

    Added Extra check when adding meta in database table.

--- app/code/P2c2p/P2c2pPayment/Helper/P2c2pMeta.php
+++ app/code/P2c2p/P2c2pPayment/Helper/P2c2pMeta.php
@@ -25,40 +25,42 @@ class P2c2pMeta
 		
 		$model = $this->objMetaFactory->create();	
 
-		$model->setData('p2c2p_id' , $request['order_id']);
-		$model->setData('order_id' , $request['order_id']);
+		$model->setData('p2c2p_id' , array_key_exists('order_id',$request) ? $request['order_id'] : '' );
+		$model->setData('order_id' , array_key_exists('order_id',$request) ? $request['order_id'] : '' );
 		$model->setData('user_id' , $user_id);
-		$model->setData('version' , $request['version']);
-		$model->setData('request_timestamp' , $request['request_timestamp']);
-		$model->setData('merchant_id' , $request['merchant_id']);
-		$model->setData('invoice_no' , $request['invoice_no']);
-		$model->setData('currency' , $request['currency']);
-		$model->setData('amount' , $request['amount']);
-		$model->setData('transaction_ref' , $request['transaction_ref']);
-		$model->setData('approval_code' , $request['approval_code']);
-		$model->setData('eci' , $request['eci']);
-		$model->setData('transaction_datetime' , $request['transaction_datetime']);
-		$model->setData('payment_channel' , $request['payment_channel']);
-		$model->setData('payment_status' , $request['payment_status']);
-		$model->setData('channel_response_code' , $request['channel_response_code']);
-		$model->setData('channel_response_desc' , $request['channel_response_desc']);
-		$model->setData('masked_pan' , $request['masked_pan']);
-		$model->setData('stored_card_unique_id' , $request['stored_card_unique_id']);
-		$model->setData('backend_invoice' , $request['backend_invoice']);
-		$model->setData('paid_channel' , $request['paid_channel']);
-		$model->setData('paid_agent' , $request['paid_agent']);
-		$model->setData('recurring_unique_id' , $request['recurring_unique_id']);
-		$model->setData('user_defined_1' , $request['user_defined_1']);
-		$model->setData('user_defined_2' , $request['user_defined_2']);
-		$model->setData('user_defined_3' , $request['user_defined_3']);
-		$model->setData('user_defined_4' , $request['user_defined_4']);
-		$model->setData('user_defined_5' , $request['user_defined_5']);
-		$model->setData('browser_info' , $request['browser_info']);
-		$model->setData('ippPeriod' , $request['ippPeriod']);
-		$model->setData('ippInterestType' , $request['ippInterestType']);
-		$model->setData('ippInterestRate' , $request['ippInterestRate']);
-		$model->setData('ippMerchantAbsorbRate' , $request['ippMerchantAbsorbRate']);
+		$model->setData('version' , array_key_exists('version',$request) ? $request['version'] : '' );
+		$model->setData('request_timestamp' , array_key_exists('request_timestamp',$request) ? $request['request_timestamp'] : '' );
+		$model->setData('merchant_id' , array_key_exists('merchant_id',$request) ? $request['merchant_id'] : '' );
+		$model->setData('invoice_no' , array_key_exists('invoice_no',$request) ? $request['invoice_no'] : '' );
+		$model->setData('currency' , array_key_exists('currency',$request) ? $request['currency'] : '' );
+		$model->setData('amount' , array_key_exists('amount',$request) ? $request['amount'] : '' );
+		$model->setData('transaction_ref' , array_key_exists('transaction_ref',$request) ? $request['transaction_ref'] : '' );
+		$model->setData('approval_code' , array_key_exists('approval_code',$request) ? $request['approval_code'] : '' );
+		$model->setData('eci' , array_key_exists('eci',$request) ? $request['eci'] : '' );
+		$model->setData('transaction_datetime' , array_key_exists('transaction_datetime',$request) ? $request['transaction_datetime'] : '' );
+		$model->setData('payment_channel' , array_key_exists('payment_channel',$request) ? $request['payment_channel'] : '' );
+		$model->setData('payment_status' , array_key_exists('payment_status',$request) ? $request['payment_status'] : '' );
+		$model->setData('channel_response_code' , array_key_exists('channel_response_code',$request) ? $request['channel_response_code'] : '' );
+		$model->setData('channel_response_desc' , array_key_exists('channel_response_desc',$request) ? $request['channel_response_desc'] : '' );
+		$model->setData('masked_pan' , array_key_exists('masked_pan',$request) ? $request['masked_pan'] : '' );
+		$model->setData('stored_card_unique_id' , array_key_exists('stored_card_unique_id',$request) ? $request['stored_card_unique_id'] : '' );
+		$model->setData('backend_invoice' , array_key_exists('backend_invoice',$request) ? $request['backend_invoice'] : '' );
+		$model->setData('paid_channel' ,  array_key_exists('paid_channel',$request) ? $request['paid_channel'] : '' );
+		$model->setData('paid_agent' , array_key_exists('paid_agent',$request) ? $request['paid_agent'] : '' );
+		$model->setData('recurring_unique_id' , array_key_exists('recurring_unique_id',$request) ? $request['recurring_unique_id'] : '' );
+		$model->setData('user_defined_1' , array_key_exists('user_defined_1',$request) ? $request['user_defined_1'] : '' );
+		$model->setData('user_defined_2' , array_key_exists('user_defined_2',$request) ? $request['user_defined_2'] : '' );
+		$model->setData('user_defined_3' , array_key_exists('user_defined_3',$request) ? $request['user_defined_3'] : '' );
+		$model->setData('user_defined_4' , array_key_exists('user_defined_4',$request) ? $request['user_defined_4'] : '' );
+		$model->setData('user_defined_5' , array_key_exists('user_defined_5',$request) ? $request['user_defined_5'] : '' );
+		$model->setData('browser_info' , array_key_exists('browser_info',$request) ? $request['browser_info'] : '' );
+		$model->setData('ippPeriod' , array_key_exists('ippPeriod',$request) ? $request['ippPeriod'] : '' );
+		$model->setData('ippInterestType' , array_key_exists('ippInterestType',$request) ? $request['ippInterestType'] : '' );
+		$model->setData('ippInterestRate' , array_key_exists('ippInterestRate',$request) ? $request['ippInterestRate'] : '' );
+		$model->setData('ippMerchantAbsorbRate' , array_key_exists('ippMerchantAbsorbRate',$request) ? $request['ippMerchantAbsorbRate'] : '' );
+
 		$model->save();
+
 	}
 
 	// save the logged in user token into p2c2p_token table for stored card payment getaway.
